ed1937294df1292418d25c45c220c1b3e9e5ade2
Malejni <malenabrinkheden@gmail.com>
2015-03-10 17:24:28 +0100

    update 1
    contact + images

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -53,7 +53,7 @@
                         <a href="#page-top"></a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#about">About</a>
+                        <a class="page-scroll" href="#about">Result</a>
                     </li>
                     
                     <li>
@@ -76,7 +76,7 @@
                         <div id="searchForm" action="" method="GET">
 
         <input type="text" name="song" placeholder="Song title..."/>
-        <a href="#about">
+        <a href="#about" class="page-scroll" >
             <input type="submit"/>
         </a>
 </div>
@@ -87,7 +87,7 @@
         </div>
     </header>
 
-    <!-- About Section -->
+    <!-- Result Section -->
     <section id="about" class="container content-section text-center">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
@@ -135,20 +135,16 @@
     <section id="contact" class="container content-section text-center">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
-                <h2>Contact Start Bootstrap</h2>
-                <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
-                <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
+                <h2>Contact SongSmoothie</h2>
+                <p>Feel free to email us to provide some feedback on our mash-up, give us suggestions for new templates and themes, or to just say hello!</p>
+                <p><a href="mailto:songsmoothie@gmail.com">songsmoothie@gmail.com</a>
                 </p>
                 <ul class="list-inline banner-social-buttons">
+                    
                     <li>
-                        <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                        <a href="https://github.com/jimmiec/mashup" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
                     </li>
-                    <li>
-                        <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                    </li>
-                    <li>
-                        <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
-                    </li>
+                    
                 </ul>
             </div>
         </div>
